43abd979726b24518ae4586e0a2b0b71f67583bf
Denis Kamenski <kengji45@gmail.com>
2017-08-17 00:03:09 +0300

    chaging speed

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
   window.onload = function(){
     var videoElem = document.querySelectorAll('.video-block');
     Array.prototype.forEach.call(videoElem, function(item){
-      var videoElem = item.querySelector('.video-block__video');
+      var videoElem = item.getElementsByClassName('video-block__video')[0];
       var playPauseBtn = item.querySelector('.video-block__play');
+      var speedBtn = item.querySelectorAll('.video-block__speed-value');
+      var curSpeedBtn;
       playPauseBtn.addEventListener('click', function(){
         this.classList.toggle('video-block__play_played');
 
@@ -13,8 +15,21 @@
           videoElem.pause();
         }
       })
-      videoElem.playbackRate = .5;
-      //
+      Array.prototype.forEach.call(speedBtn, function(btn){
+        btn.addEventListener('click',function(){
+          if(this.classList.contains('video-block__speed-value--selected')){
+            return false;
+          }
+          curSpeedBtn = this;
+          for( i = 0; i < speedBtn.length; i++){
+            speedBtn[i].classList.remove('video-block__speed-value--selected')
+          }
+          curSpeedBtn.classList.add('video-block__speed-value--selected');
+          var speed = parseFloat(curSpeedBtn.getAttribute('data-speed'));
+          videoElem.playBackRate = speed;
+          console.log(videoElem.playBackRate)
+        })
+      })
     })
   }
 })()
